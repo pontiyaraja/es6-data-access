ef651b03b468d057b7cd103bf0520e3dce9b6091
Pontiyaraja <ramamoorthypandiyaraja@gmail.com>
2017-12-29 17:46:40 +0530

    added promis and bable script

--- js/app.js
+++ js/app.js
@@ -1,35 +1,34 @@
 // XMLHttpRequest wrapper using callbacks
-let request = (obj, successHandler, errorHandler) => {
+let request = obj => {
+  return new Promise((resolve, reject) => {
     let xhr = new XMLHttpRequest();
     xhr.open(obj.method || "GET", obj.url);
     if (obj.headers) {
-        Object.keys(obj.headers).forEach(function(key) {
+        Object.keys(obj.headers).forEach(key => {
             xhr.setRequestHeader(key, obj.headers[key]);
         });
     }
     xhr.onload = () => {
         if (xhr.status >= 200 && xhr.status < 300) {
-            successHandler(xhr.response);
+            resolve(xhr.response);
         } else {
-            errorHandler(xhr.statusText);
+            reject(xhr.statusText);
         }
     };
-    xhr.onerror = () => {
-        errorHandler(xhr.statusText);
-    };
+    xhr.onerror = () => reject(xhr.statusText);
     xhr.send(obj.body);
+  });
 }
 
-request({url:"employees.json"},
-    (data) => {
+request({url:"employees.json"})
+    .then(data => {
         let employees = JSON.parse(data);
         let html = "";
-        employees.forEach((employee) => html +=`
-        <div><img src=${employee.picture}><div>${employee.firstName} ${employee.lastName}<p>${employee.phone}</p></div></div>`
-        );
+        employees.forEach(employee => {html +=`
+        <div><img src='${employee.picture}'/><div>${employee.firstName} ${employee.lastName}<p>${employee.phone}</p></div></div>`
+      });
         document.getElementById("list").innerHTML = html;
-    },
-     (error) => {
+    })
+     .catch(error => {
         console.log(error);
-    }
-);
+    });
